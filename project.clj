59816de2e3e9df91666c3efa9169518a012013a6
Hoàng Minh Thắng <p@banphim.net>
2014-01-18 23:50:26 +0700

    bump to v0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-bower "0.2.0-SNAPSHOT"
+(defproject lein-bower "0.2.0"
   :description "Manage Bower dependencies for CLJS projects"
   :url "https://github.com/myguidingstar/lein-bower"
   :license {:name "Apache License, version 2.0"
