ab1841613d889138c8cf1f5363abb194c495837e
Hoàng Minh Thắng <p@banphim.net>
2014-04-08 07:43:57 +0700

    update deps and bump to v0.3.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject lein-bower "0.2.0"
+(defproject lein-bower "0.3.0"
   :description "Manage Bower dependencies for CLJS projects"
   :url "https://github.com/myguidingstar/lein-bower"
   :license {:name "Apache License, version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[lein-npm "0.2.0"]]
+  :dependencies [[lein-npm "0.3.0"]]
   :eval-in-leiningen true)
