02c2408a947195b86d06f60826de3228fa162aad
Artem Chernyak <artemchernyak@gmail.com>
2016-10-17 10:16:20 -0500

    Bump to v0.5.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-bower "0.5.1"
+(defproject lein-bower "0.5.2"
   :description "Manage Bower dependencies for CLJS projects"
   :url "https://github.com/myguidingstar/lein-bower"
   :license {:name "Apache License, version 2.0"
